b31cade9c61ef8c33ac4c06a7600f3002001c29b
Peter Mount <peter@retep.org>
2018-02-14 15:54:43 +0000

    darwind3 microservice

---
+++ bin/darwind3/main.go
@@ -0,0 +1,38 @@
+// darwind3 Microservice
+package main
+
+import (
+  "bin"
+  "darwind3"
+  "log"
+)
+
+func main() {
+  log.Println( "darwind3 v0.1" )
+
+  bin.RunApplication( app )
+}
+
+func app( config *bin.Config ) ( func(), error ) {
+
+  d3 := &darwind3.DarwinD3{}
+
+  config.DbPath( &config.Database.PushPort, "dwd3.db" )
+
+  if err := d3.OpenDB( config.Database.PushPort ); err != nil {
+    return nil, err
+  }
+
+  if config.D3.ResolveSched {
+    // Allow D3 to resolve schedules from the timetable
+    d3.Timetable = config.Services.Timetable
+  }
+
+  d3.RegisterRest( config.Server.Ctx )
+
+  if config.RabbitMQ.Url != "" {
+    d3.BindConsumer( &config.RabbitMQ, config.D3.QueueName, config.D3.RoutingKey )
+  }
+
+  return nil, nil
+}
