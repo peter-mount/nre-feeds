78a2d4a92650ee650cc5c6f3dafc02c839bdca6b
Peter Mount <peter@retep.org>
2018-06-06 23:26:02 +0100

    Special case that darwintt service is the darwintimetable package

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -5,6 +5,12 @@
 DEST=$1
 BIN=$2
 
+PACKAGE=$BIN
+if [ "$PACKAGE" = "darwintt" ]
+then
+  PACKAGE="darwintimetable"
+fi
+
 echo "Building ${BIN}"
-go build -o ${DEST} github.com/peter-mount/nre-feeds/${BIN}/bin
+go build -o ${DEST} github.com/peter-mount/nre-feeds/${PACKAGE}/bin
 #go build -o ${DEST} github.com/peter-mount/nre-feeds/bin/${BIN}
