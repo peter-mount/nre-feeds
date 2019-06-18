6cfd3742b95ebbabcb3f691129e2f23ea849dfbd
peter <peter@retep.org>
2019-06-18 19:17:47 +0100

    Retrieve referene data on startup

--- darwinref/update/service.go
+++ darwinref/update/service.go
@@ -1,46 +1,62 @@
 package update
 
 import (
-	"github.com/peter-mount/golib/kernel"
-	"github.com/peter-mount/nre-feeds/bin"
-	"github.com/peter-mount/nre-feeds/darwind3"
-	"github.com/peter-mount/nre-feeds/darwinref/service"
+  "github.com/peter-mount/golib/kernel"
+  "github.com/peter-mount/golib/kernel/logger"
+  "github.com/peter-mount/nre-feeds/bin"
+  "github.com/peter-mount/nre-feeds/darwind3"
+  "github.com/peter-mount/nre-feeds/darwinref/service"
 )
 
 type ReferenceUpdateService struct {
-	config *bin.Config
-	ref    *service.DarwinRefService
+  config *bin.Config
+  ref    *service.DarwinRefService
+  logger *logger.LoggerService
 }
 
 func (a *ReferenceUpdateService) Name() string {
-	return "ReferenceUpdateService"
+  return "ReferenceUpdateService"
 }
 
 func (a *ReferenceUpdateService) Init(k *kernel.Kernel) error {
-	svce, err := k.AddService(&bin.Config{})
-	if err != nil {
-		return err
-	}
-	a.config = (svce).(*bin.Config)
+  svce, err := k.AddService(&bin.Config{})
+  if err != nil {
+    return err
+  }
+  a.config = (svce).(*bin.Config)
 
-	svce, err = k.AddService(&service.DarwinRefService{})
-	if err != nil {
-		return err
-	}
-	a.ref = (svce).(*service.DarwinRefService)
+  svce, err = k.AddService(&service.DarwinRefService{})
+  if err != nil {
+    return err
+  }
+  a.ref = (svce).(*service.DarwinRefService)
 
-	return nil
+  svce, err = k.AddService(&logger.LoggerService{})
+  if err != nil {
+    return err
+  }
+  a.logger = (svce).(*logger.LoggerService)
+
+  return nil
 }
 
 func (a *ReferenceUpdateService) Start() error {
-	// Only listen if S3 is enabled
-	if a.config.S3.Enabled {
-		a.config.RabbitMQ.ConnectionName = "darwin ref"
-		a.config.RabbitMQ.Connect()
+  // Only listen if S3 is enabled
+  if a.config.S3.Enabled {
+    a.config.RabbitMQ.ConnectionName = "darwin ref"
+    err := a.config.RabbitMQ.Connect()
+    if err != nil {
+      return err
+    }
 
-		em := darwind3.NewDarwinEventManager(&a.config.RabbitMQ, a.config.D3.EventKeyPrefix)
-		em.ListenToEvents(darwind3.Event_TimeTableUpdate, a.referenceUpdateListener)
-	}
+    em := darwind3.NewDarwinEventManager(&a.config.RabbitMQ, a.config.D3.EventKeyPrefix)
+    err = em.ListenToEvents(darwind3.Event_TimeTableUpdate, a.referenceUpdateListener)
+    if err != nil {
+      return err
+    }
 
-	return nil
+    go a.findUpdates()
+  }
+
+  return nil
 }
--- darwinref/update/updateTimetable.go
+++ darwinref/update/updateTimetable.go
@@ -1,88 +1,159 @@
 package update
 
 import (
-	"compress/gzip"
-	"encoding/xml"
-	"github.com/peter-mount/nre-feeds/darwind3"
-	"log"
-	"os"
+  "compress/gzip"
+  "encoding/xml"
+  "github.com/aws/aws-sdk-go/aws"
+  "github.com/aws/aws-sdk-go/aws/credentials"
+  "github.com/aws/aws-sdk-go/aws/session"
+  "github.com/aws/aws-sdk-go/service/s3"
+  "github.com/peter-mount/golib/kernel/logger"
+  "github.com/peter-mount/nre-feeds/darwind3"
+  "log"
+  "os"
+  "strings"
 )
 
 const (
-	tempFile = "/tmp/inbound.xml.gz"
+  tempFile = "/tmp/inbound.xml.gz"
 )
 
 // timetableUpdateListener listens for real time updates for when new reference
 // data is made available.
 func (d *ReferenceUpdateService) updateTimetable(tid *darwind3.TimeTableId) error {
-	fname := tid.TTRefFile
+  if strings.Contains(tid.TTRefFile, "ref_v3") {
+    return nil
+  }
 
-	err := d.retrieveReference(fname)
-	if err != nil {
-		return err
-	}
+  return d.logger.Report(
+    "NRDP Reference Update",
+    func(log *logger.Logger) error {
 
-	err = d.uploadFile(tid)
-	if err != nil {
-		return err
-	}
+      log.Printf("New reference %s", tid.TimeTableId)
 
-	err = d.importReference(tid.TimeTableId, fname)
-	if err != nil {
-		return err
-	}
+      fname := tid.TTRefFile
 
-	return nil
+      err := d.retrieveReference(log, fname)
+      if err != nil {
+        return err
+      }
+
+      err = d.uploadFile(log, tid)
+      if err != nil {
+        return err
+      }
+
+      err = d.importReference(log, tid.TimeTableId, fname)
+      if err != nil {
+        return err
+      }
+
+      return nil
+    })
 }
 
-func (d *ReferenceUpdateService) retrieveReference(fname string) error {
-	file, err := os.Create(tempFile)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
+func (d *ReferenceUpdateService) retrieveReference(log *logger.Logger, fname string) error {
+  file, err := os.Create(tempFile)
+  if err != nil {
+    return err
+  }
+  defer file.Close()
 
-	return d.config.S3.RetrieveFile(fname, file)
+  return d.config.S3.RetrieveFile(log, fname, file)
 }
 
-func (d *ReferenceUpdateService) importReference(id, fname string) error {
-	file, err := os.Open(tempFile)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
+func (d *ReferenceUpdateService) importReference(log *logger.Logger, id, fname string) error {
+  file, err := os.Open(tempFile)
+  if err != nil {
+    return err
+  }
+  defer file.Close()
 
-	gr, err := gzip.NewReader(file)
-	if err != nil {
-		return err
-	}
+  gr, err := gzip.NewReader(file)
+  if err != nil {
+    return err
+  }
 
-	log.Println("Importing", id)
-	err = xml.NewDecoder(gr).Decode(d.ref.GetDarwinReference())
-	if err != nil {
-		return err
-	}
+  log.Println("Importing", id)
+  err = xml.NewDecoder(gr).Decode(d.ref.GetDarwinReference())
+  if err != nil {
+    return err
+  }
 
-	return nil
+  return nil
 }
 
-func (d *ReferenceUpdateService) uploadFile(tid *darwind3.TimeTableId) error {
-	if d.config.Upload.Enabled {
-		path, err := tid.GetPath()
-		if err != nil {
-			return err
-		}
+func (d *ReferenceUpdateService) uploadFile(log *logger.Logger, tid *darwind3.TimeTableId) error {
+  if d.config.Upload.Enabled {
+    path, err := tid.GetPath()
+    if err != nil {
+      return err
+    }
 
-		file, err := os.Open(tempFile)
-		if err != nil {
-			return err
-		}
-		defer file.Close()
+    file, err := os.Open(tempFile)
+    if err != nil {
+      return err
+    }
+    defer file.Close()
 
-		err = d.config.Upload.UploadFile(file, path+tid.TTRefFile)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
+    err = d.config.Upload.UploadFile(log, file, path+tid.TTRefFile)
+    if err != nil {
+      return err
+    }
+  }
+  return nil
+}
+
+func (d *ReferenceUpdateService) findUpdates() {
+  log.Println("Looking for updates")
+
+  config := d.config.S3
+
+  sess, err := session.NewSession(
+    &aws.Config{
+      Region: aws.String(config.Region),
+      Credentials: credentials.NewStaticCredentials(
+        config.AccessKey,
+        config.SecretKey,
+        "",
+      ),
+    })
+  if err != nil {
+    log.Println("Failed to create aws session", err)
+  }
+
+  client := s3.New(sess)
+
+  input := &s3.ListObjectsInput{
+    Bucket:  aws.String(config.Bucket),
+    MaxKeys: aws.Int64(256),
+  }
+
+  objs, err := client.ListObjects(input)
+  if err != nil {
+    log.Println("Failed to find updates", err)
+  }
+
+  var file string
+  for _, obj := range objs.Contents {
+    if strings.Contains(*obj.Key, "ref") {
+      file = *obj.Key
+    }
+  }
+
+  if file != "" {
+    if strings.HasPrefix(file, config.Path) {
+      file = file[len(config.Path):]
+    }
+
+    id := file
+    if i := strings.Index(id, "_"); i > -1 {
+      id = id[:i]
+    }
+
+    _ = d.updateTimetable(&darwind3.TimeTableId{
+      TimeTableId: id,
+      TTRefFile:   file,
+    })
+  }
 }
