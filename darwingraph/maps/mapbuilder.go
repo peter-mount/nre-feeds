826a4568cf5bde34b1af7df74334cbdac67e267b
peter <peter@retep.org>
2021-05-20 09:42:20 +0100

    Fix path's that go outside the bounds of the plot. For us they caused horrible lines as the transform exceeded the plot

--- darwingraph/maps/mapbuilder.go
+++ darwingraph/maps/mapbuilder.go
@@ -2,6 +2,7 @@ package maps
 
 import (
 	sm "github.com/flopp/go-staticmaps"
+	"github.com/fogleman/gg"
 	"github.com/golang/geo/s2"
 	"github.com/peter-mount/nre-feeds/darwingraph"
 	"image/color"
@@ -59,8 +60,48 @@ func (m *MapBuilder) Render(w io.Writer) error {
 	return png.Encode(w, img)
 }
 
+type wrapperPath struct {
+	p *sm.Path
+}
+
+func (o wrapperPath) Bounds() s2.Rect { return o.p.Bounds() }
+func (o wrapperPath) ExtraMarginPixels() (float64, float64, float64, float64) {
+	return o.p.ExtraMarginPixels()
+}
+func (o wrapperPath) Draw(gc *gg.Context, trans *sm.Transformer) {
+	if len(o.p.Positions) <= 1 {
+		return
+	}
+
+	first := true
+
+	gc.ClearPath()
+	gc.SetLineWidth(o.p.Weight)
+	gc.SetLineCap(gg.LineCapRound)
+	gc.SetLineJoin(gg.LineJoinRound)
+	for _, ll := range o.p.Positions {
+		x, y := trans.LatLngToXY(ll)
+		if x < 0 || y < 0 || int(x) >= gc.Width() || int(y) >= gc.Height() {
+			first = true
+		} else if first {
+			gc.MoveTo(x, y)
+			first = false
+		} else {
+			gc.LineTo(x, y)
+		}
+	}
+	gc.SetColor(o.p.Color)
+	gc.Stroke()
+}
+
 func (m *MapBuilder) AddObject(o sm.MapObject) *MapBuilder {
-	m.ctx.AddObject(o)
+	if o != nil {
+		if p, ok := o.(*sm.Path); ok {
+			m.ctx.AddObject(&wrapperPath{p: p})
+		} else {
+			m.ctx.AddObject(o)
+		}
+	}
 	return m
 }
 
@@ -74,7 +115,7 @@ func (m *MapBuilder) ForEachStationNode(d *darwingraph.DarwinGraph, f func(*MapB
 func (m *MapBuilder) AppendStation(s *darwingraph.StationNode) *MapBuilder {
 	s.ForEachTiploc(func(t *darwingraph.TiplocNode) {
 		if t.HasPosition() {
-			m.ctx.AddObject(sm.NewCircle(
+			m.AddObject(sm.NewCircle(
 				s2.LatLngFromDegrees(float64(t.Lat), float64(t.Lon)),
 				color.RGBA{R: 0xff, A: 0xff},
 				color.RGBA{R: 0xff, A: 0xff},
@@ -94,25 +135,28 @@ func (m *MapBuilder) ForEachStationEdge(d *darwingraph.DarwinGraph, f func(*MapB
 }
 
 func (m *MapBuilder) AppendStationEdge(s *darwingraph.StationEdge) *MapBuilder {
-	var a s2.LatLng
-	first := true
+	var a s2.LatLng    // Previous point
+	var ar []s2.LatLng // Slice of points forming this segment
 	s.ForEachTiploc(func(t *darwingraph.TiplocNode) {
 		if t.HasPosition() {
 			ll := s2.LatLngFromDegrees(float64(t.Lat), float64(t.Lon))
-			if first {
-				first = false
-			} else if ll.Distance(a).Degrees() > 0.2 {
-				first = true
-			} else {
-				m.AddObject(sm.NewPath([]s2.LatLng{a, ll}, color.RGBA{A: 0xff}, 1))
+
+			// Ignore lines longer than .3 degrees - cuts out some major errors in the data
+			if len(ar) > 0 && ll.Distance(a).Degrees() > 0.3 {
+				m.appendEdge(ar) // Append existing segment up to this point
+				ar = nil         // Start a new slice
 			}
-			a = ll
+
+			ar = append(ar, ll) // Append new point to segment
+			a = ll              // copy for distance check against next point
 		}
 	})
+	m.appendEdge(ar)
 	return m
 }
 
-func (m *MapBuilder) appendEdge(s *darwingraph.StationEdge, p []s2.LatLng) []s2.LatLng {
-	m.AddObject(sm.NewPath(p, color.RGBA{A: 0xff}, 1))
-	return nil
+func (m *MapBuilder) appendEdge(p []s2.LatLng) {
+	if len(p) > 1 {
+		m.AddObject(sm.NewPath(p, color.RGBA{A: 0xff}, 1))
+	}
 }
